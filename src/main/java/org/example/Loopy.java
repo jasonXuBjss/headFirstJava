7dc6c576d529f4f03c65a7d4e1801f3d2bc82419
Jason Xu <jason.xu@bjss.com>
2023-07-16 18:54:00 +0100

    Initial commit

---
+++ src/main/java/org/example/Loopy.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Loopy{
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 3) {
+            System.out.printf("Doo");
+            System.out.printf("Bee");
+            x = x  + 1;
+        }
+
+        if (x ==3 ) {
+            System.out.printf("Do");
+        }
+    }
+}
